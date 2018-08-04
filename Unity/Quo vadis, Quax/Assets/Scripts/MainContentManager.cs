e57411e9ab58c05b947cb6de3f737d1c7609114b
Jonas <contact@atalantus.de>
2018-08-04 20:32:22 +0200

    Implemented Loading Image in Unity project

---
+++ Unity/Quo vadis, Quax/Assets/Scripts/MainContentManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainContentManager : MonoBehaviour
+{
+    [SerializeField] private LoadImage _loadImage;
+    [SerializeField] private GameObject _noContentPanel;
+    [SerializeField] private Text _noContentText;
+
+    private void Awake()
+    {
+        _loadImage.UpdatedLoadingState += OnLoadingState_Changed;
+    }
+
+    private void OnLoadingState_Changed(LoadImage.LoadingState state)
+    {
+        switch (state)
+        {
+            case LoadImage.LoadingState.NOT_LOADING:
+                _noContentPanel.SetActive(true);
+                _noContentText.text = "NO MAP LOADED!";
+                break;
+            case LoadImage.LoadingState.LOADING:
+                _noContentPanel.SetActive(true);
+                _noContentText.text = "PROCESSING IMAGE...";
+                break;
+            case LoadImage.LoadingState.FAILED:
+                _noContentPanel.SetActive(true);
+                _noContentText.text = "FAILED LOADING THE MAP!";
+                break;
+            case LoadImage.LoadingState.DONE:
+                _noContentPanel.SetActive(false);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("state", state, null);
+        }
+    }
+}
---
+++ Unity/Quo vadis, Quax/Assets/Scripts/UI/LoadImage.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Crosstales.FB;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadImage : MonoBehaviour
+{
+    public delegate void LoadingMapEventHandler(LoadingState state);
+
+    public enum LoadingState
+    {
+        NOT_LOADING,
+        LOADING,
+        FAILED,
+        DONE
+    }
+
+    private Animator _animator;
+    [SerializeField] private Text _filePathText;
+    public event LoadingMapEventHandler UpdatedLoadingState;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void OpenFile()
+    {
+#if CT_FB
+        var path = FileBrowser.OpenSingleFile("Open Quax Map", "", "png");
+        if (path != string.Empty)
+        {
+            if (ProcessImage(path))
+            {
+                _filePathText.text = path;
+                if (UpdatedLoadingState != null)
+                    UpdatedLoadingState.Invoke(LoadingState.LOADING);
+            }
+            else
+            {
+                if (UpdatedLoadingState != null)
+                    UpdatedLoadingState.Invoke(LoadingState.FAILED);
+            }
+        }
+        else
+        {
+            _filePathText.text = "<b>No File Loaded!</b>";
+            if (UpdatedLoadingState != null)
+                UpdatedLoadingState.Invoke(LoadingState.NOT_LOADING);
+        }
+#else
+        Debug.LogWarning("File Browser NOT installed!");
+#endif
+    }
+
+    public void TogglePanel()
+    {
+        _animator.SetTrigger("Toggle");
+    }
+
+    private bool ProcessImage(string imagePath)
+    {
+        // TODO: Process Image
+        return true;
+    }
+}
