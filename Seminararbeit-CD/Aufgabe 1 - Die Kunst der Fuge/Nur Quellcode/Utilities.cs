6c4eeb061966868dd276236b388697d36747df7f
Jonas <contact@atalantus.de>
2018-10-31 15:43:13 +0100

    Added Aufgabe 1 to Seminararbeit-CD

---
+++ Seminararbeit-CD/Aufgabe 1 - Die Kunst der Fuge/Nur Quellcode/Utilities.cs
@@ -0,0 +1,22 @@
+﻿namespace Aufgabe1_DieKunstDerFuge
+{
+    /// <summary>
+    /// Helper methods.
+    /// </summary>
+    public static class Utilities
+    {
+        /// <summary>
+        /// Fills an Array with a given value.
+        /// </summary>
+        /// <typeparam name="T">Type of the Array.</typeparam>
+        /// <param name="array">The Array.</param>
+        /// <param name="value">The value.</param>
+        public static void FillArray<T>(this T[] array, T value)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+        }
+    }
+}
